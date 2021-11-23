0dc14ab4fe6644f77e79e1dc1608db48f6f598f3
FocusCookie <stephan_lueddemann@web.de>
2021-11-23 13:21:44 +0100

    Added auth0 flow

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import logo from "./logo.svg";
 import "./styles/App.css";
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { loginWithRedirect, isAuthenticated, logout, user } = useAuth0();
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +19,22 @@ function App() {
         >
           Learn React
         </a>
+
+        <button onClick={() => loginWithRedirect()}>Login</button>
+
+        {isAuthenticated && (
+          <button onClick={() => logout({ returnTo: window.location.origin })}>
+            Log Out
+          </button>
+        )}
+
+        {isAuthenticated && (
+          <div>
+            <img src={user.picture} alt={user.name} />
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+          </div>
+        )}
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,10 +3,17 @@ import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="storyswap.eu.auth0.com"
+      clientId="NQlPOqwgKF0MYycRgZ2k3SMcUPTpMgiQ"
+      redirectUri={window.location.origin}
+    >
+      <App />
+    </Auth0Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
