1b3d46088e10904b1d72fe67ca7455d553c6310d
FocusCookie <stephan_lueddemann@web.de>
2021-12-13 16:15:08 +0100

    Fixed modal story. Not using random unsplash image, because that will fail the screeenshot testing. Its not the same unsplash image.

--- src/components/Modal/Modal.stories.js
+++ src/components/Modal/Modal.stories.js
@@ -6,10 +6,13 @@ export default {
   component: Modal,
 };
 
+const unsplashUrl =
+  "https://images.unsplash.com/photo-1507842217343-583bb7270b66?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1590&q=80";
+
 const Template = (args) => (
   <div
     className="w-screen h-screen"
-    style={{ backgroundImage: "url(https://source.unsplash.com/800x600/)" }}
+    style={{ backgroundImage: `url(${unsplashUrl})` }}
   >
     <Modal {...args}>content in modal</Modal>
   </div>
