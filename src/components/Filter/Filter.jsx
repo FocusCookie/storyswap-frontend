213d5bfac5c27b69da48a18b9f44e05cb951e320
FocusCookie <stephan_lueddemann@web.de>
2021-12-13 19:39:59 +0100

    First state implementation

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -7,13 +7,42 @@ import { Button } from "../Button/Button";
 
 export const Filter = ({ onFilter, ...props }) => {
   const [open, setOpen] = useState(false);
+  const [filters, setFilters] = useState({});
 
   function handleOpen() {
     setOpen((open) => !open);
   }
+
   function handleClose() {
     setOpen((open) => !open);
   }
+
+  function handleResetFilter() {
+    setFilters({});
+  }
+
+  function handleNewFilter(filter) {
+    const currentFilters = Object.assign({}, filters);
+    const key = Object.keys(filter)[0];
+
+    // remove filter if value is empty
+    if (filter[key] === "" && currentFilters[key]) {
+      delete currentFilters[key];
+    }
+
+    const newFilter =
+      filter[key] === "" ? currentFilters : { ...currentFilters, ...filter };
+
+    setFilters(newFilter);
+  }
+
+  function handleFilter() {
+    onFilter(filters);
+  }
+
+  //TODO: Uncontrolled inputs gerade, values müssen gegeben werden. also doch isbn etc als eigene states und mit use effect zusammenbauen als gesamt filter
+
+  //TODO: wenn filter active diese mittels badge icon (zahl in kreis ) im filter öffnen button einblenden
 
   return (
     <div className="filter">
@@ -27,12 +56,18 @@ export const Filter = ({ onFilter, ...props }) => {
 
       {open && (
         <Card className="filter__content">
-          <Input onChange={(v) => console.log(v)} label="ISBN" />
-          <Input onChange={(v) => console.log(v)} label="Author" />
-          <Input onChange={(v) => console.log(v)} label="Postleitzahl" />
-          <Input onChange={(v) => console.log(v)} label="Stadt" />
-          <Button onClick={() => console.log("filter")}>Filtern</Button>
-          <Button size="sm" variant="text" onClick={() => console.log("reset")}>
+          <Input onChange={(v) => handleNewFilter({ isbn: v })} label="ISBN" />
+          <Input
+            onChange={(v) => handleNewFilter({ author: v })}
+            label="Author"
+          />
+          <Input
+            onChange={(v) => handleNewFilter({ zip: v })}
+            label="Postleitzahl"
+          />
+          <Input onChange={(v) => handleNewFilter({ city: v })} label="Stadt" />
+          <Button onClick={handleFilter}>Filtern</Button>
+          <Button size="sm" variant="text" onClick={handleResetFilter}>
             zurücksetzen
           </Button>
           <Button variant="secondary" onClick={handleClose}>
