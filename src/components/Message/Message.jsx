f53f8389488d41da699b643f09dde5962f7313ec
FocusCookie <stephan_lueddemann@web.de>
2021-12-20 09:05:32 +0100

    Added message component

---
+++ src/components/Message/Message.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Message.css";
+import { Card } from "../Card/Card";
+
+export const Message = ({ message, timestamp, creator, ...props }) => {
+  return (
+    <div
+      className={`message ${
+        creator ? "message--justify-end" : "message--justify-start"
+      }`}
+    >
+      <Card
+        className={`message__card ${creator ? "message--primary" : ""}`}
+        {...props}
+      >
+        <p class="message__content">{message}</p>
+        <p className="message__timestamp">{timestamp}</p>
+      </Card>
+    </div>
+  );
+};
+
+Message.propTypes = {
+  /**
+   * the actual message
+   */
+  message: PropTypes.string,
+  /**
+   * timestamp of the message
+   */
+  timestamp: PropTypes.string,
+  /**
+   * the user is the creator of the message
+   */
+  creator: PropTypes.bool,
+};
+
+Message.defaultProps = {
+  message: "",
+  created: "",
+  creator: false,
+};
